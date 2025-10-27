2f49d7fcd80245981c57057c99e296d63402480c
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2025-10-27 10:57:57 +0000

    Polished intro and other bits

--- R/AlphaPart.R
+++ R/AlphaPart.R
@@ -20,9 +20,9 @@
 #' main function AlphaPart for partitioning genetic values and auxiliary
 #' functions for manipulating data and summarizing, visualizing, and saving
 #' results.
-#' 
-#' Please see the introductory vignette for instructions for using this package. 
-#' The vignette can be viewed using the following command: 
+#'
+#' Please see the introductory vignette for instructions for using this package.
+#' The vignette can be viewed using the following command:
 #' \code{vignette("intro",package="AlphaPart")}
 #' @keywords internal
 "_PACKAGE"
@@ -34,7 +34,7 @@
 #'   al. (2008): Partition of the genetic trend to validate multiple
 #'   selection decisions. Animal: an international journal of animal
 #'   bioscience. DOI: \doi{10.1017/S175173110800205X}
-#'   
+#'
 #' @param x data.frame, with (at least) the following columns:
 #'   individual, father, and mother identif ication, and year of birth;
 #'   see arguments \code{colId}, \code{colFid}, \code{colMid},
@@ -77,11 +77,11 @@
 #'
 #' @details Pedigree in \code{x} must be valid in a sense that there
 #'   are:
-#'   
-#'   * no directed loops (the simplest example is that the individual 
-#'   identification is equal to the identification of a father or mother) 
-#'   * no bisexuality, e.g., fathers most not appear as mothers 
-#'   * father and/or mother can be unknown (missing) - defined with 
+#'
+#'   * no directed loops (the simplest example is that the individual
+#'   identification is equal to the identification of a father or mother)
+#'   * no bisexuality, e.g., fathers most not appear as mothers
+#'   * father and/or mother can be unknown (missing) - defined with
 #'   any "code" that is different from existing identifications
 #'
 #' Unknown (missing) values for breeding values are propagated down the
@@ -124,42 +124,59 @@
 #'   \code{summaryAlphaPart} class).  Class \code{AlphaPart} is a
 #'   list. The first \code{length(colBV)} components (one for each trait
 #'   and named with trait label, say trt) are data frames. Each
-#'   data.frame contains: 
-#'   
-#'   * `x` columns from initial data `x` 
-#'   * `trt_pa` parent average 
+#'   data.frame contains:
+#'
+#'   * `x` columns from initial data `x`
+#'   * `trt_pa` parent average
 #'   * `trt_ms` Mendelian sampling term
 #'   * `trt_path1, trt_path2, ...` breeding value partitions
 #'
 #' The last component of returned object is also a list named
 #' \code{info} with the following components holding meta information
-#' about the analysis: 
-#' 
+#' about the analysis:
+#'
 #' * `path` column name holding path information
-#' * `nP` number of paths 
+#' * `nP` number of paths
 #' * `lP` path labels
 #' * `nT` number of traits
-#' * `lT` trait labels 
+#' * `lT` trait labels
 #' * `warn` potential warning messages associated with this object
 #'
 #' If \code{colBy!=NULL} the resulting object is of a class
 #' \code{summaryAlphaPart}, see
 #' \code{\link[AlphaPart]{summary.AlphaPart}} for details.
-#' 
+#'
 #' @example inst/examples/examples_AlphaPart.R
 #' @export
-AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
-                      sort=TRUE, verbose=1, pedType="IPP", colId=1,
-                      colFid=2, colMid=3, colPath=4, colBV=5:ncol(x),
-                      colBy=NULL) {
+AlphaPart <- function(
+  x,
+  pathNA = FALSE,
+  recode = TRUE,
+  unknown = NA,
+  sort = TRUE,
+  verbose = 1,
+  pedType = "IPP",
+  colId = 1,
+  colFid = 2,
+  colMid = 3,
+  colPath = 4,
+  colBV = 5:ncol(x),
+  colBy = NULL
+) {
   ## Test if the data is a data.frame
-  if(is_tibble(x)){
+  if (is_tibble(x)) {
     x <- as.data.frame(x)
   }
   ## --- Setup ---
-  test <- (length(colId) > 1 | length(colFid) > 1 | length(colMid) > 1 | length(colPath) > 1 | length(colBy) > 1)
+  test <- (length(colId) > 1 |
+    length(colFid) > 1 |
+    length(colMid) > 1 |
+    length(colPath) > 1 |
+    length(colBy) > 1)
   if (test) {
-    stop("arguments 'colId', 'colFid', 'colMid', 'colPath', and 'colBy' must be of length 1")
+    stop(
+      "arguments 'colId', 'colFid', 'colMid', 'colPath', and 'colBy' must be of length 1"
+    )
   }
 
   if (is.null(colBy)) {
@@ -174,42 +191,42 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
   }
 
   # -- Test identification
-  if(!is.numeric(colId)){
+  if (!is.numeric(colId)) {
     colId <- which(colnames(x) %in% colId)
-    if (length(colId)==0) {
+    if (length(colId) == 0) {
       stop("Identification not valid for 'colId' column name", call. = FALSE)
-    } 
+    }
   }
-  if(!is.numeric(colMid)){
+  if (!is.numeric(colMid)) {
     colMid <- which(colnames(x) %in% colMid)
-    if (length(colMid)==0) {
+    if (length(colMid) == 0) {
       stop("Identification not valid for 'colMid' column name", call. = FALSE)
     }
   }
-  if(!is.numeric(colFid)){
+  if (!is.numeric(colFid)) {
     colFid <- which(colnames(x) %in% colFid)
-    if (length(colFid)==0) {
+    if (length(colFid) == 0) {
       stop("Identification not valid for 'colFid' column name", call. = FALSE)
     }
   }
-  if(!is.numeric(colPath)){
+  if (!is.numeric(colPath)) {
     testN <- length(colPath)
     colPath <- which(colnames(x) %in% colPath)
-    if (length(colPath)!=testN) {
+    if (length(colPath) != testN) {
       stop("Identification not valid for 'colPath' column name", call. = FALSE)
     }
-    testN <-  NULL # not needed anymore
+    testN <- NULL # not needed anymore
   }
-  if(!is.numeric(colBy)){
+  if (!is.numeric(colBy)) {
     testN <- length(colBy)
     colByOriginal <- colBy
     colBy <- which(colnames(x) %in% colBy)
-    if (length(colBy)!=testN) {
+    if (length(colBy) != testN) {
       stop("Identification not valid for 'colBy' column name", call. = FALSE)
     }
     testN <- NULL # not needed anymore
   }
-  if(!is.numeric(colBV)){
+  if (!is.numeric(colBV)) {
     testN <- length(colBV)
     colBV <- which(colnames(x) %in% colBV)
     if (length(colBV) != testN) {
@@ -222,7 +239,9 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
   ## Make sure that identifications are numeric if  recode=FALSE
   test <- !sapply(x[, c(colId, colFid, colMid)], is.numeric) & !recode
   if (any(test)) {
-    stop("argument 'recode' must be 'TRUE' when identif ications in 'x' are not numeric")
+    stop(
+      "argument 'recode' must be 'TRUE' when identif ications in 'x' are not numeric"
+    )
   }
 
   ## Make sure that colBV columns are numeric
@@ -235,7 +254,7 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
   ## Sort so that parents precede children
   if (sort) {
     recode <- TRUE
-    x <- x[order(orderPed(ped=x[, c(colId, colFid, colMid)])), ]
+    x <- x[order(orderPed(ped = x[, c(colId, colFid, colMid)])), ]
   }
 
   # TODO: Strip out all centering and scaling in favour of
@@ -248,61 +267,74 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
     if (!missing(scaleEBV)) {
       controlvals[names(scaleEBV)] <- scaleEBV
     }
-    if(controlvals$center == TRUE | controlvals$scale == TRUE){
-      x[, colBV] <- sGV(y = x[,c(colId, colFid, colMid, colBV)], 
-                         center = controlvals$center, 
-                         scale = controlvals$scale, 
-                         recode = recode, unknown = unknown)
+    if (controlvals$center == TRUE | controlvals$scale == TRUE) {
+      x[, colBV] <- sGV(
+        y = x[, c(colId, colFid, colMid, colBV)],
+        center = controlvals$center,
+        scale = controlvals$scale,
+        recode = recode,
+        unknown = unknown
+      )
     }
   }
 
   ## Recode all ids to 1:n
   if (recode) {
-    y <- cbind(id=seq_len(nrow(x)),
-               fid=match(x[, colFid], x[, colId], nomatch=0),
-               mid=match(x[, colMid], x[, colId], nomatch=0))
-    colnames(y) <- c(colId,colFid,colMid)
+    y <- cbind(
+      id = seq_len(nrow(x)),
+      fid = match(x[, colFid], x[, colId], nomatch = 0),
+      mid = match(x[, colMid], x[, colId], nomatch = 0)
+    )
+    colnames(y) <- c(colId, colFid, colMid)
   } else {
     y <- as.matrix(x[, c(colId, colFid, colMid)])
     ## Make sure we have 0 when recoded data is provided
     if (is.na(unknown)) {
-      y[, c(colFid, colMid)] <- NAToUnknown(x=y[, c(colFid, colMid)],
-                                            unknown=0)
+      y[, c(colFid, colMid)] <- NAToUnknown(
+        x = y[, c(colFid, colMid)],
+        unknown = 0
+      )
     } else {
-      if (unknown != 0)  {
+      if (unknown != 0) {
         y[, c(colFid, colMid)] <-
-          NAToUnknown(x=unknownToNA(x=y[, c(colFid, colMid)],
-                                    unknown=unknown), unknown=0)
+          NAToUnknown(
+            x = unknownToNA(x = y[, c(colFid, colMid)], unknown = unknown),
+            unknown = 0
+          )
       }
     }
   }
   y <- cbind(y, as.matrix(x[, colBV]))
-  
+
   ## Test if father and mother codes precede children code -
   ## computational engine needs this
   test <- y[, 2] >= y[, 1]
   if (any(test)) {
     print(x[test, ])
     print(sum(test))
-    stop("sorting/recoding problem: parent (father in this case) code must preceede children code - use arguments 'sort' and/or 'recode'")
+    stop(
+      "sorting/recoding problem: parent (father in this case) code must preceede children code - use arguments 'sort' and/or 'recode'"
+    )
   }
 
   test <- y[, 3] >= y[, 1]
   if (any(test)) {
     print(x[test, ])
     print(sum(test))
-    stop("sorting/recoding problem: parent (mother in this case) code must preceede children code - use arguments 'sort' and/or 'recode'")
+    stop(
+      "sorting/recoding problem: parent (mother in this case) code must preceede children code - use arguments 'sort' and/or 'recode'"
+    )
   }
 
   ## --- Dimensions and Paths ---
   ## Pedigree size
   nI <- nrow(x)
   ## Traits
-  lT <- colnames(x[, colBV, drop=FALSE])
+  lT <- colnames(x[, colBV, drop = FALSE])
   nT <- length(lT) # number of traits
   colnames(y)[4:ncol(y)] <- lT
   ## Missing values
-  nNA <- apply(x[, colBV, drop=FALSE], 2, function(z) sum(is.na(z)))
+  nNA <- apply(x[, colBV, drop = FALSE], 2, function(z) sum(is.na(z)))
   names(nNA) <- lT
   ## Paths - P matrix
   test <- is.na(x[, colPath])
@@ -314,7 +346,9 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
       stop("unknown (missing) value for path not allowed; use 'pathNA=TRUE'")
     }
   }
-  if (!is.factor(x[, colPath])) x[, colPath] <- factor(x[, colPath])
+  if (!is.factor(x[, colPath])) {
+    x[, colPath] <- factor(x[, colPath])
+  }
   lP <- levels(x[, colPath])
   nP <- length(lP) # number of paths
   P <- as.integer(x[, colPath]) - 1
@@ -330,7 +364,9 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
         stop("unknown (missing) value for group not allowed; use 'pathNA=TRUE'")
       }
     }
-    if (!is.factor(x[, colBy])) x[, colBy] <- factor(x[, colBy])
+    if (!is.factor(x[, colBy])) {
+      x[, colBy] <- factor(x[, colBy])
+    }
     lG <- levels(x[, colBy])
     nG <- length(lG)
     g <- as.integer(x[, colBy])
@@ -339,16 +375,35 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
   if (verbose > 0) {
     cat("\nSize:\n")
     cat(" - individuals:", nI, "\n")
-    cat(" - traits: ", nT, " (", paste(lT, collapse=", "), ")", "\n", sep="")
-    cat(" - paths: ",  nP, " (", paste(lP, collapse=", "), ")", "\n", sep="")
+    cat(
+      " - traits: ",
+      nT,
+      " (",
+      paste(lT, collapse = ", "),
+      ")",
+      "\n",
+      sep = ""
+    )
+    cat(" - paths: ", nP, " (", paste(lP, collapse = ", "), ")", "\n", sep = "")
     if (groupSummary) {
-      cat(" - groups: ", nG, " (", paste(lG, collapse=", "), ")", "\n", sep="")
+      cat(
+        " - groups: ",
+        nG,
+        " (",
+        paste(lG, collapse = ", "),
+        ")",
+        "\n",
+        sep = ""
+      )
     }
     cat(" - unknown (missing) values:\n")
     print(nNA)
   }
 
-  if (any(nNA > 0)) stop("unknown (missing) values are propagated through the pedigree and therefore not allowed")
+  if (any(nNA > 0))
+    stop(
+      "unknown (missing) values are propagated through the pedigree and therefore not allowed"
+    )
   nNA <- NULL # not needed anymore
 
   ## --- Compute ---
@@ -366,41 +421,57 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
 
   ## Compute
   if (!groupSummary) {
-    tmp <- .Call("AlphaPartDrop",
-                 c1_=c1, c2_=c2,
-                 nI_=nI, nP_=nP, nT_=nT,
-                 y_=y, 
-                 P_=P, Px_=cumsum(c(0, rep(nP, nT-1))),
-                 PACKAGE="AlphaPart")
+    tmp <- .Call(
+      "AlphaPartDrop",
+      c1_ = c1,
+      c2_ = c2,
+      nI_ = nI,
+      nP_ = nP,
+      nT_ = nT,
+      y_ = y,
+      P_ = P,
+      Px_ = cumsum(c(0, rep(nP, nT - 1))),
+      PACKAGE = "AlphaPart"
+    )
   } else {
-    N <- aggregate(x=y[-1, -c(1:3)], by=list(by=x[, colBy]), FUN=length)
-    tmp <- vector(mode="list", length=3)
+    N <- aggregate(x = y[-1, -c(1:3)], by = list(by = x[, colBy]), FUN = length)
+    tmp <- vector(mode = "list", length = 3)
     names(tmp) <- c("pa", "ms", "xa")
-    tmp$pa <- tmp$ms <- matrix(data=0, nrow=nG+1, ncol=nT)
-    tmp$xa <- .Call("AlphaPartDropGroup",
-                 c1_=c1, c2_=c2,
-                 nI_=nI, nP_=nP, nT_=nT, nG_=nG,
-                 y_=y, P_=P, Px_=cumsum(c(0, rep(nP, nT-1))), g_=g,
-                 PACKAGE="AlphaPart")
+    tmp$pa <- tmp$ms <- matrix(data = 0, nrow = nG + 1, ncol = nT)
+    tmp$xa <- .Call(
+      "AlphaPartDropGroup",
+      c1_ = c1,
+      c2_ = c2,
+      nI_ = nI,
+      nP_ = nP,
+      nT_ = nT,
+      nG_ = nG,
+      y_ = y,
+      P_ = P,
+      Px_ = cumsum(c(0, rep(nP, nT - 1))),
+      g_ = g,
+      PACKAGE = "AlphaPart"
+    )
   }
 
   ## Assign nice column names
-  colnames(tmp$pa) <- paste(lT, "_pa", sep="")
-  colnames(tmp$ms) <- paste(lT, "_ms", sep="")
-  colnames(tmp$xa) <- c(t(outer(lT, lP, paste, sep="_")))
+  colnames(tmp$pa) <- paste(lT, "_pa", sep = "")
+  colnames(tmp$ms) <- paste(lT, "_ms", sep = "")
+  colnames(tmp$xa) <- c(t(outer(lT, lP, paste, sep = "_")))
 
   ## --- Massage results ---
 
   ## Put partitions for one trait in one object (-1 is for removal of
   ## the "zero" row)
-  ret <- vector(mode="list", length=nT+1)
+  ret <- vector(mode = "list", length = nT + 1)
   t <- 0
   colP <- colnames(tmp$pa)
   colM <- colnames(tmp$ms)
   colX <- colnames(tmp$xa)
 
-  for (j in 1:nT) { ## j <- 1
-    Py <- seq(t+1, t+nP)
+  for (j in 1:nT) {
+    ## j <- 1
+    Py <- seq(t + 1, t + nP)
     ret[[j]] <- cbind(tmp$pa[-1, j], tmp$ms[-1, j], tmp$xa[-1, Py])
     colnames(ret[[j]]) <- c(colP[j], colM[j], colX[Py])
     t <- max(Py)
@@ -414,12 +485,15 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
       ## Hassle in order to get all columns and to be able to work with
       ##   numeric or character column "names"
       colX <- colX2 <- colnames(x)
-      names(colX) <- colX; names(colX2) <- colX2
+      names(colX) <- colX
+      names(colX2) <- colX2
       ## ... put current agv in the last column in original data
       colX <- c(colX[!(colX %in% colX[colBV[i]])], colX[colBV[i]])
       ## ... remove other traits
-      colX <- colX[!(colX %in% colX2[(colX2 %in% colX2[colBV]) & !
-                                       (colX2 %in% colX2[colBV[i]])])]
+      colX <- colX[
+        !(colX %in%
+          colX2[(colX2 %in% colX2[colBV]) & !(colX2 %in% colX2[colBV[i]])])
+      ]
       ret[[i]] <- cbind(x[, colX], as.data.frame(ret[[i]]))
       rownames(ret[[i]]) <- NULL
     }
@@ -427,9 +501,16 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
 
   ## Additional (meta) info. on number of traits and paths for other
   ## methods
-  tmp <- colnames(x); names(tmp) <- tmp
-  ret[[nT+1]] <- list(path=tmp[colPath], nP=nP, lP=lP, nT=nT, lT=lT,
-                      warn=NULL)
+  tmp <- colnames(x)
+  names(tmp) <- tmp
+  ret[[nT + 1]] <- list(
+    path = tmp[colPath],
+    nP = nP,
+    lP = lP,
+    nT = nT,
+    lT = lT,
+    warn = NULL
+  )
   ## names(ret)[nT+1] <- "info"
   names(ret) <- c(lT, "info")
 
@@ -439,7 +520,7 @@ AlphaPart <- function(x, pathNA=FALSE, recode=TRUE, unknown=NA,
   if (groupSummary) {
     ret$by <- colByOriginal
     ret$N <- N
-    summary(object=ret, sums=TRUE)
+    summary(object = ret, sums = TRUE)
   } else {
     ret
   }
